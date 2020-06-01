a395e152c52c1a74007c08e407b14850f0bb6831
ardus-uk <peter@ardus.co.uk>
2020-06-01 10:06:24 +0100

    add file writing

--- get_noun.py
+++ get_noun.py
@@ -1,14 +1,30 @@
 #!/usr/bin/python3
 
 import random
+import string
 
-filename = "./nouns.txt"
-with open(filename,'r') as reader:
+in_filename = "./nouns.txt"
+with open(in_filename,'r') as reader:
     whole_text = reader.read()
 
 words_list = whole_text.split()
 index  = random.randint(0, len(words_list)-1)
 print(words_list[index])
+
+long_words = []
+for word in words_list:
+    if len(word) > 6:
+        long_words.append(word)
+
+print(long_words)
+long_words_in_string = "\n".join(long_words)
+print(long_words_in_string)
+
+
+out_filename = "./longwords.txt"
+with open(out_filename, 'w') as writer:
+    writer.write(long_words_in_string)
+    
 
 
 
