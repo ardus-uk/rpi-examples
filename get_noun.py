84a5da0751bf7d042c441b8ebfeece627ade8d29
ardus-uk <peter@ardus.co.uk>
2020-06-01 10:14:13 +0100

    remove unnecessary imports from get_noun

--- get_noun.py
+++ get_noun.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 
-import random
-import string
+#import random
 
 in_filename = "./nouns.txt"
 with open(in_filename,'r') as reader:
     whole_text = reader.read()
 
 words_list = whole_text.split()
-index  = random.randint(0, len(words_list)-1)
-print(words_list[index])
+# index  = random.randint(0, len(words_list)-1)
+# print(words_list[index])
 
 long_words = []
 for word in words_list:
@@ -20,11 +19,6 @@ print(long_words)
 long_words_in_string = "\n".join(long_words)
 print(long_words_in_string)
 
-
 out_filename = "./longwords.txt"
 with open(out_filename, 'w') as writer:
     writer.write(long_words_in_string)
-    
-
-
-
