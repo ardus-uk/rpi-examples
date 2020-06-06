d47a8f3bc3b4fefa829a2d97f7fd1defeed086ae
ardus-uk <peter@ardus.co.uk>
2020-06-06 18:14:16 +0100

    More concise if expressions

--- hangmanPN.py
+++ hangmanPN.py
@@ -6,17 +6,13 @@ def players_char(prompt_string):
     message = prompt_string
     while (message !=""):
         character = input(message)
-        if (len(character)!=1):
-            message = "One letter only, please.\n" + prompt_string
-        elif ((ord(character) not in range(65,91)) and (ord(character) not in range(97,123))):
-            message = "Only alphabetic characters, please.\n" + prompt_string
-        else:
-            message = ""
+        if (len(character)!=1): message = "One letter only, please.\n" + prompt_string
+        elif ((ord(character) not in range(65,91)) and (ord(character) not in range(97,123))): message = "Only alphabetic characters, please.\n" + prompt_string
+        else: message = ""
     return character.upper()
     
 def word_from_wordlist(filename):
-    with open(filename,'r') as reader:
-        whole_text = reader.read()
+    with open(filename,'r') as reader: whole_text = reader.read()
     words_list = whole_text.split()
     random_index  = random.randint(0, len(words_list)-1)
     return words_list[random_index].upper()
@@ -39,7 +35,8 @@ msg_number = 0
 while (msg_number < 3):
     print(" ".join(wsf))
     ch = players_char("Your choice of letter?: ")
-    if ((ch in fails) or (ch in wsf)): msg_number = 0
+    if ((ch in fails) or (ch in wsf)):
+        msg_number = 0
     elif ch not in target_word:
         fails.append(ch)
         msg_number = 1 if (len(fails) < MAX_WRONG_GUESSES) else 4
