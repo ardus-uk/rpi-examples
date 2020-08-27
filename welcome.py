116a331d5ff62fcf73fcf4f666b923b5bd2382f5
ardus-uk <peter@ardus.co.uk>
2020-08-27 20:40:04 +0100

    Add welcome scripts

---
+++ welcome.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def greet(lst):
+    for name in lst:
+        print ("Welcome back, " + name)
+
+def otherfunction():
+    print("Foobar")
+
+
---
+++ welcome2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import welcome
+
+memberlist = [  'Peter',
+                'Mark',
+                'Liz',
+                'Dave',
+                'John',
+                'Tom',
+            ]
+
+#print(memberlist)
+#print(sorted(memberlist))
+
+welcome.greet(sorted(memberlist))
+
