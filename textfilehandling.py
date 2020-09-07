497a4620c55f9fbee0d5afc1452d11296bae81dc
ardus-uk <peter@ardus.co.uk>
2020-09-07 13:11:12 +0100

    Add September2020 exercises

---
+++ textfilehandling.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+with open("longwords.txt") as givenfile:
+     contents = givenfile.read()
+
+# Write a program to find out how many words there are.  
+# The output should be "There are <n> words in the file", with the correct number in place of <n>.
+
+words = contents.split('\n')
+print('There are ',len(words),' words in the file')
+
+# Write a program to create a new text file which consists of all the words in the original file, 
+# but in alphabetic order with no line breaks (ie the words all on the same line), 
+# separated by single spaces.
+
+outputfilename = "ex2.txt"
+with open(outputfilename,'w') as outputfile:
+     outputfile.write(' '.join(sorted(words)))
+print("File '"+outputfilename+"' now contains the words in alphabetic order")
+
+# Write a program to find the longest word in the file.
+# (If there are two or more of the same length, choose the first in alphabetic order).
+
+longest_word = ''
+for word in sorted(words):
+     if len(word) > len(longest_word):
+          longest_word = word
+print ("The longest word is ", longest_word)
+
+# Write a program to create a new text file which consists of 
+# those words in the original file which contain 6 or more letters.
+
+outputfilename = "ex4.txt"
+with open(outputfilename,'w') as outputfile:
+     for word in words:
+          if len(word) >= 6:
+               outputfile.write(word + '\n')
+print("File '"+outputfilename+"' now contains the words of length 6 or greater")
+
+# Write a program to find out the mean (average) length of the words in the file.   
+# The output should be of the form
+# "The average number of letters in these words is <x>"  
+# where <x> is given to 2 decimal places accuracy.
+
+total_chars = sum(len(word) for word in words)
+average_word_length = total_chars/len(words)
+print(f'The average number of letters in these words is {average_word_length:.2f}')
+
+#  Write a program to find all those words in the file which are palindromes
+
+palindromes = []
+for word in words:
+     if word == word[::-1]: palindromes.append(word)
+print("The list of palindromes, in alphabetical order, is ",sorted(palindromes))
+
