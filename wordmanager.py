cd59c39d8688b97e076a79d3f22d2ddd15b5684c
ardus-uk <peter@ardus.co.uk>
2021-11-08 18:27:54 +0000

    First pass at solving problem

--- wordmanager.py
+++ wordmanager.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python3
 import json
-import os
-from itertools import combinations
+import string
+import enchant
+import sys
 
-script_directory = os.path.dirname(os.path.abspath(__file__))
+given_word = str(sys.argv[1])
+d = enchant.Dict("en_GB")
 
-test = False
-jw = os.path.join(script_directory, "words_dictionary.json")
+def check_count(gw,k):
+    c = True
+    for char in set(gw):
+        c = c and gw.count(char) >= k.count(char)
+    return c
+
+jw = "/home/peter/Coding/rpi-examples/words_dictionary.json"
 
 with open(jw) as words_file:
-    english_words = json.load(words_file)
+    eng_words = json.load(words_file)
 
-print('frzate' in english_words)
-print(len(english_words))
+alphabet_list = list(string.ascii_lowercase)
 
-anag = sorted('liquorice')
-print(anag)
-for word in english_words:
-    if sorted(word)==anag:
-        print(word)
-
-set = "abcde"
-for l in range(0, len(set) + 1):
-    for subset in itertools.combinations(letters, l): #all combinations in subset
-        list_comb.append(subset)
+residue_set = set(alphabet_list)-set(list(given_word))
+c1 = {k: v for k, v in eng_words.items() if len(k)>=3 and len(k)<=len(given_word)}
+c2 = {k: v for k, v in c1.items() if len(set(list(k)).intersection(residue_set))==0}
+c3 = {k: v for k, v in c2.items() if given_word[0] in list(k)}
+c4 = {k: v for k, v in c3.items() if check_count(given_word,k)}
+l_c4 = sorted(sorted(list(c4.keys())),key=len)
+for a in l_c4:
+    if d.check(a):
+        print(a)
