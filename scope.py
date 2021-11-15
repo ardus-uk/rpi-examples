4743cc4784cb2ffe842ba7930c6fe9d9c050a898
ardus-uk <peter@ardus.co.uk>
2021-11-15 12:14:31 +0000

    Add more details to scope example

--- scope.py
+++ scope.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 
+# An example to illustrate global and local variables
+# Peter Normington
+# 2021-11-15
+
+# the id() function returns the identity of the object; 
+# this is an integer that is unique for the given object
+# and remains constant during its lifetime. 
+
+# EVERYTHING IS AN OBJECT
+# even simple integers like 0, 1, 2 ...
+print("Id of 0 is", id(0))
+print("Id of 1 is", id(1))
+print("Id of 3 is", id(3))
+print("------------------------")
+print()
+
 counter = 0
+# now, the variable "counter" refers to the object "0"
 
 def fn_use_global():
     global counter
